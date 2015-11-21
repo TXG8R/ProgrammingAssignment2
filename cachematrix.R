3bfbd415371059028121c16dc2f4b3aa75ccc99d
TXG8R <joelvweber@gmail.com>
2015-11-20 21:32:58 -0500

    Final Working Version with Complete Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
+## Abstract / Executive Summary
+## The makeCacheMatrix function caches/persists matrix and inverse matrix values through set 
+## functions and returns those matrices through corresponding get functions.
+
+## The cacheSolve requests the inverse of a matrix stored/cached in the makeCacheMatrix
+## function.
+
+## If the matrix returned  by the makeCacheMatrix$getinvMatrix function is a NON-EMPTY matrix 
+## the cacheSolve function then prints a message indicating that a cached (inverse) matrix 
+## was returned and then terminates its own execution by returning the cached inverse matrix.
+
+## If the matrix returned by the makeCacheMatrix$getinvMatrix function is an EMPTY matrix 
+## then the cacheSolve fucntion:
+## 1) Requests the matrix to be inverted by way of the makeCacheMatrix$getMatrix function
+## 2) Solves for the inverted matrix by way of the base R solve() function
+## 3) Caches the solution by way of the makeCacheMatrix$setinvMatrix function
+
+##########################################################################
 ## FUNCTION NAME: makeCacheMatrix
 ## FUNCTION PURPOSE: To store matrix and inverse matrix values so that they may
 ##                   be randomly accessed (returned) by other functions and code blocks
 ##                   outside of the makeCacheMatrix.  In other words, this function
 ##                   maintains the state of matrix and inverse matrix values.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
   ## Instantiate an empty (0,0) matrix
   invMatrix <- matrix(nrow = 0, ncol = 0)
   
@@ -37,6 +55,7 @@ makeCacheMatrix <- function(x = matrix()) {
        getinvMatrix = getinvMatrix)
 }
 
+##########################################################################
 ## FUNCTION NAME: cacheSolve
 ## FUNCTION PURPOSE: Return a matrix that is the inverse of 'x'
 ##                   If the inverse matrix value/property from the makeCacheMatrix function
